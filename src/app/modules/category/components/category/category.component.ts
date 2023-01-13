f70c2e2c5502556320a28331f32eccef0ee2d14e
Esteban Ahumada <epunamun_@hotmail.com>
2023-01-13 00:55:22 -0300

    Se configuro la tabla ng container y ademas se hizo la conexion con el backend para obtener los datos de las categorias ya guardadas

--- src/app/modules/category/components/category/category.component.ts
+++ src/app/modules/category/components/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { CategoryService } from 'src/app/modules/shared/services/category.service';
 
 @Component({
@@ -14,16 +15,45 @@ export class CategoryComponent implements OnInit {
     this.getCategories();
   }
 
+  displayedColumns: string [] = ['id', 'name', 'description', 'actions'];
+  dataSource = new MatTableDataSource<CategoryElement>();
+
   getCategories(){
 
-    this.categoryService.getCategories()
-      .subscribe( (data:any) => {
+    this.categoryService.getCategories().subscribe(
+       (data:any) => {
         
         console.log("Respuesta categories: ", data);
+        this.processCategoriesResponse(data);
 
-      }, (error:any) => {
+      }, (error: any) => {
         console.log("error: ", error);
       })
   }
 
+  processCategoriesResponse(resp: any){
+
+    const dataCategory: CategoryElement[] = [];
+
+    if( resp.metadata[0].code == "00") {
+      
+      let listCategory = resp.categoryResponse.category;
+
+      listCategory.forEach((element: CategoryElement) => {
+        dataCategory.push(element); 
+        
+      });
+
+      this.dataSource = new MatTableDataSource<CategoryElement>(dataCategory);
+
+    }
+
+  }
+
+}
+
+export interface CategoryElement {
+  description: string;
+  id: number;
+  name: string;
 }
